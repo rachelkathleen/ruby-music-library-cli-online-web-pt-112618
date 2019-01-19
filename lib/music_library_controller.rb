bc253712b84f2d984af4699248bc8a357508333f
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-19 03:12:10 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -53,10 +53,40 @@ class MusicLibraryController
   end
 
   def list_genres
-    Genre.all.sort{|a, b| a.name <=> b.name}.each_with_index(1) do |g, index|
+    Genre.all.sort{|a, b| a.name <=> b.name}.each.with_index(1) do |g, index|
       puts "#{index}. #{g.name}"
    end
  end
 
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.chomp
+    if artist = Artist.find_by_name(input)
+    artist.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+      puts "#{i}. #{s.name} - #{s.genre.name}"
+      end
+    end
+  end
 
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.chomp
+
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+        puts "#{i}. #{s.artist.name} - #{s.name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+
+    input = gets.chomp.to_i
+    if (1..Song.all.length).include?(input)
+      song = Song.all.sort{|a, b| a.name <=> b.name}[input - 1]
+    end
+
+    puts "Playing #{song.name} by #{song.artist.name}" if song
+  end
 end
